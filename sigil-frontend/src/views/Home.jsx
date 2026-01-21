bbbd677f09a0b71183766ecbb564bc04403a6dd0
purgalo <olasz.g48@gmail.com>
2026-01-21 16:41:12 +0100

    added another color

--- sigil-frontend/src/views/Home.jsx
+++ sigil-frontend/src/views/Home.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
   return (
     <>
         
-        <div>
+        <div className="bg-secondary-bg text-parchment">
         <h1>Welcome to the Home Page</h1>
         <p>This is the main landing page of the application.</p>
             
