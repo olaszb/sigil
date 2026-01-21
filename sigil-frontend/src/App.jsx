e71437323bb84807fb8a660707d40cf40ee0b31f
purgalo <olasz.g48@gmail.com>
2026-01-21 16:12:05 +0100

    Created a not found page

--- sigil-frontend/src/App.jsx
+++ sigil-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { PrivateGuard, PublicGuard } from './util/Guard';
 import { AuthProvider } from './contexts/AuthContext';
 import Dashboard from './views/Dashboard';
 import MainLayout from './util/MainLayout';
+import NotFoundPage from './views/NotFound';
 
 function App() {
 
@@ -27,6 +28,9 @@ function App() {
           <Route element={<PublicGuard/>}>
             <Route path="/login" element={<LoginPage />} />
           </Route>
+
+          {/* Fallback */}
+          <Route path="*" element={<NotFoundPage />} />
 
         </Routes>
       </BrowserRouter>
