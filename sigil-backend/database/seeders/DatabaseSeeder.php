3b095924ac543f8d023422004f0240e777329e73
purgalo <olasz.g48@gmail.com>
2026-01-25 00:58:32 +0100

    Created factories for venues and events and implemented the seeder for testing

--- sigil-backend/database/seeders/DatabaseSeeder.php
+++ sigil-backend/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use App\Models\User;
+use App\Models\Venue;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -22,6 +24,23 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => Hash::make('Test1234'),
+        ]);
+
+        Venue::factory()->create([
+            'name' => 'Grand Hall',
+            'address' => '123 Main St',
+            'city' => 'Metropolis',
+            'country' => 'Freedonia',
+            'postal_code' => '12345',
+            'capacity' => 500,
+        ]);
+
+        Event::factory()->create([
+            'venue_id' => 1,
+            'organizer_id' => 1,
+            'title' => 'Spring Festival',
+            'description' => 'A celebration of spring with music and food.',
+            'start_time' => now()->addMonths(2),
         ]);
     }
 }
